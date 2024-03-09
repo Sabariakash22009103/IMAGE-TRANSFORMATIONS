c3021d0aa83addb20dd82c2adbaef3eaad326cba
Sabariakash22009103 <akashsabari002@gmail.com>
2024-03-09 14:05:05 +0530

    added

--- IMAGE TRANSFORMATION[4-6].py
+++ IMAGE TRANSFORMATION[4-6].py
@@ -4,15 +4,20 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 input_image=cv2.imread("got.jpg") 
-input_image=cv2.cvtColor(input_image,) 
+input_image=cv2.cvtColor(input_image,cv2.COLOR_BGR2RGB) 
 plt.axis("off") 
 plt.imshow(input_image)
 plt.show()
-rows, cols, dim = 
-M_x=np.float
+rows, cols, dim = input_image.shape
+M_x=np.float32([[1,0,0 ],
+                [0,-1,rows],
+                [0,0,1 ]])
+M_y=np.float32([[-1,0,cols ],
+                [0,1,0],
+                [0,0,1 ]])
 reflected_img_xaxis=cv2.warpPerspective(input_image,M_x,(cols,rows))
 reflected_img_yaxis=cv2.warpPerspective(input_image,M_y,(cols,rows))
-plt.imshow(reflected_)
+plt.imshow(reflected_img_xaxis)
 plt.show()
 
 ##v)Image Rotation:
@@ -23,8 +28,9 @@ input_image=cv2.imread("got.jpg")
 input_image=cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
 angle=np.radians(45)
 M=np.float32([[np.cos(angle),-(np.sin(angle)),0],
-               [np.
-rotated_img=
+               [np.sin(angle),(np.cos(angle)),0],
+               [0,0,1]])
+rotated_img=cv2.warpPerspective(input_image, M, (int(cols),int(rows)))
 
 ##vi)Image Cropping:
 
@@ -32,6 +38,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 input_image=cv2.imread("got.jpg") 
-input_image=cv2.cvtColor(input_image,
-plt.show()
+input_image=cv2.cvtColor(input_image,cv2.COLOR_BGR2RGB)
+cropped_img=input_image[100:300, 100:300]
+plt.imshow(cropped_img)
 
--- Image transformations[1-3].py
+++ Image transformations[1-3].py
@@ -3,8 +3,8 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-input_img=cv2.imread("color image of flower.jpg")
-input_img=cv2.cvtColor(input_img,cv2.)
+input_img=cv2.imread("download.jpeg")
+input_img=cv2.cvtColor(input_img,cv2.COLOR_BGR2RGB)
 plt.axis('off')
 plt.imshow(input_img)
 plt.show()
@@ -12,13 +12,17 @@ rows,cols,dim=input_img.shape
 M=np.float32([[1,0,20],
              [0,1,50],
              [0,0,1]])
-translated_img=cv2.
+translated_img=cv2.warpPerspective(input_img,M,(cols, rows))
 plt.axis('off')
-
+plt.imshow(translated_img)
+plt.show
 
 
 ##ii)Image Scaling
-
+rows, cols, dim=img.shape
+M=np.float32([[1.5,0,0],
+             [0,1.8],
+             [0,0,1]])
 scaled_img=cv2.warpPerspective(input_img,M,(cols,rows))
 plt.axis('off')
 plt.imshow(scaled_img)
@@ -29,9 +33,11 @@ plt.show()
 M_x=np.float32([[1,0.2,0],
                [0,1,0],
                [0,0,1]])
-M_y=np.
+M_y=np.float32([[1, 0, 0],
+                [0.5, 1, 0],
+                [0,0,1]])
 sheared_img_xaxis=cv2.warpPerspective(input_img,M_x,(cols,rows))
-sheared_img_yaxis=
+sheared_img_yaxis=cv2.warpPerspective(input_img,M_y,(cols,rows))
 plt.axis('off')
 plt.imshow(sheared_img_xaxis)
 plt.show()
